6faf315826a2b962985cca5a3f34e0d5300452c9
Joni <johnniecasual@gmail.com>
2022-01-30 13:09:44 +0100

    feat: initial React boilerplate structure
    Added initial React Boilerplate structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <>
+            <h1 className="heading">Casual React Boilerplate</h1>
+            <h4 className="sub-heading">Continue from here...</h4>
+        </>
+    )
+}
+
+export default App
